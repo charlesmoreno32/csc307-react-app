ecdd6b94985c8d10a2a8e32b190e56b9810ff9a5
charlesmoreno32 <charlesmoreno007@gmail.com>
2023-09-27 09:58:30 -0700

    Part II Step 4

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,5 +1,5 @@
 // src/MyApp.js
-import React from "react";
+import React, {useState} from 'react';
 import Table from "./Table";
 
 const characters = [
@@ -27,12 +27,31 @@ const [characters, setCharacters] = useState([
     name: 'Charlie',
     job: 'Janitor',
   },
-  // the rest of the data
+  {
+    name: "Mac",
+    job: "Bouncer",
+  },
+  {
+    name: "Dee",
+    job: "Aspring actress",
+  },
+  {
+    name: "Dennis",
+    job: "Bartender",
+  },
 ]); 
+
+function removeOneCharacter (index) {
+  const updated = characters.filter((character, i) => {
+      return i !== index
+  });
+setCharacters(updated);
+}
   
 return (
 <div className="container">
-    <Table characterData={characters} />
+    <Table characterData={characters} 
+    removeCharacter={removeOneCharacter} />
    </div>
 ) 
 }
